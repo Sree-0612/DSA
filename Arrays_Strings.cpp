d1b2a0db7039f6b0f674f5c208f75849fa21a3bf
Sreenidhi R <154693924+Sree-0612@users.noreply.github.com>
2024-08-05 23:48:25 +0530

    Add files via upload

---
+++ Arrays_Strings.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //1D array
+    // int arr[5];
+    // cin>>arr[0]>>arr[1];
+    // cout<<"The output is:"<<arr[0]<<endl;
+
+
+    //2D Array
+    // int arr[3][5];
+    // arr[1][5] = 143;
+    // cout<<arr[1][5];
+
+    //Strings
+    // string s;
+    // s = "Sreenidhi";
+    // cout<<s[6]<<endl;
+    // int s_len = s.length();
+    // cout<<s_len<<endl;
+    // cout<<s[s_len-1] ;
+
+    //do-while loop
+    
+    int i=2;
+    do{
+        cout<<"Sreenidhi "<<i<<endl;
+        i = i+1;
+    }while(i<=1);
+
+    cout<<i<<endl;
+    
+
+
+    return 0;
+}
